28d67f89a1f510ec095be7f12510cc1179d54938
jose carlos florido cobo <kddhd.jgfc@gmail.com>
2025-12-01 11:32:37 +0100

    configuración render

--- ufc_app/settings.py
+++ ufc_app/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'middleware/security.SecurityMiddleware',
+    'middleware.security.MyCustomMiddleware'
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
